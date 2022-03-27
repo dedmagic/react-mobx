bf3d99f59c071077eb4083e204f3681dc4e0d28a
dedmagic <dedmagic@gmail.com>
2022-03-27 10:23:03 +0500

    add bootstrap 5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Learn React and Hello world! :)
                 </a>
             </header>
         </div>
