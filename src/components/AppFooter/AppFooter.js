1c42ae644339d83b16315a0b61ad42a69f68362e
dedmagic <dedmagic@gmail.com>
2022-03-30 07:52:02 +0500

    add 'react-bootstrap' and finish with footer

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,11 +1,21 @@
+import { Navbar, Container } from 'react-bootstrap';
+
 function AppFooter() {
     return (
+        //#region
         // TODO: Стили хардкодом, переделать на Bootstrap
-        <footer style={{ position: 'absolute', bottom: 0 }} >
-            <div>
-                <span>dedmagic, 2022</span>
-            </div>
-        </footer>
+        //     <footer style={{ position: 'absolute', bottom: 0 }} >
+        //         <div>
+        //             <span>dedmagic, 2022</span>
+        //         </div>
+        //     </footer>
+        //#endregion
+        // <Navbar sticky='bottom'>
+        <Navbar fixed='bottom' bg='secondary' variant='dark'>
+            {/* <Container> */}
+            <Navbar.Brand className='ms-4'>dedmagic, 2022</Navbar.Brand>
+            {/* </Container> */}
+        </Navbar>
     );
 }
 
